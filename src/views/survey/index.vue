cf9084aeb79026f93c05159420646a96d6e31bab
righthan <2753875841@qq.com>
2023-12-03 22:43:51 +0800

    first commit

---
+++ src/views/survey/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>调查管理</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/trainApplication/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>培训申请管理</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
